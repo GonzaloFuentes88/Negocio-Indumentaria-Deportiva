efc6d8193913d4c4e9c10ac375bec063e2416438
Gonzalo <fuentesgonzalo88@gmail.com>
2023-04-19 20:12:24 -0300

    Agregamos la clase UsuarioController

---
+++ NegocioIndumentariaDeportiva/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NegocioIndumentariaDeportiva.Datos;
+using NegocioIndumentariaDeportiva.Datos.DaoImpl;
+
+namespace NegocioIndumentariaDeportiva.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDao usuarioDao = new UsuarioDao();
+        
+        // GET: Usuario
+        public ActionResult Listar()
+        {
+
+            var oList = usuarioDao.FindAll();
+            return View(oList);
+        }
+    }
+}
