8f60682712eda7ef3b86e6616a40219e5a237f06
Gonzalo <fuentesgonzalo88@gmail.com>
2023-04-19 20:13:50 -0300

    Se agregan los modelos Role y Usuario

---
+++ NegocioIndumentariaDeportiva/Models/UsuarioModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NegocioIndumentariaDeportiva.Models
+{
+    public class UsuarioModel
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _nombre;
+
+        public string  Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        private string _username;
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        private Role _role;
+
+        public Role Role
+        {
+            get { return _role; }
+            set { _role = value; }
+        }
+
+        public UsuarioModel() { }
+
+
+    }
+}
