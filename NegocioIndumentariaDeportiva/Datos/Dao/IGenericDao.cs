e7795f8163a6ff0727bbd66ce07b165ea105304d
Gonzalo <fuentesgonzalo88@gmail.com>
2023-04-19 20:12:40 -0300

    Agregamos la clase generica de Dao's

---
+++ NegocioIndumentariaDeportiva/Datos/Dao/IGenericDao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NegocioIndumentariaDeportiva.Datos
+{
+    /// <summary>
+    /// Interface generica para implementar Dao's sobre los distintos obj del proyecto
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IGenericDao<T>
+    {
+        List<T> FindAll();
+        T FindOne(int id);
+
+        bool Save();
+
+        bool Delete();
+
+    }
+}
