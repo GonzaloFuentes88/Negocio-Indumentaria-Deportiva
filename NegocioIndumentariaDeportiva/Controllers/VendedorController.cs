83244682c8c946b96fa528d05c471e901a0d9a78
Juan Sein <juansein@outlook.com>
2023-04-24 00:19:01 -0300

    Se agrego el controlador del vendedor y la vista registrar venta

---
+++ NegocioIndumentariaDeportiva/Controllers/VendedorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NegocioIndumentariaDeportiva.Controllers
+{
+    public class VendedorController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult RegistrarVenta()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult RegistrarProducto()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult EditarProducto()
+        {
+            return View();
+        }
+
+
+    }
+}
