cf02bd735e77ab01ce5b50d20342825efc10a58f
Gonzalo <fuentesgonzalo88@gmail.com>
2023-05-24 19:45:44 -0300

    Modificamos la clase conexion

--- DAL/Datos/Conexion.cs
+++ DAL/Datos/Conexion.cs
@@ -69,7 +69,102 @@ namespace DAL.Datos
             objConexion.Close();
             objConexion.Dispose();
         }
-        
+
+        public DataTable LeerPorStoreProcedure(string pNombreStoreProcedure, SqlParameter[] pParametrosSql = null)
+        {
+            //Instancio un objeto del tipo DataTable
+            var unaTabla = new DataTable();
+
+            //Instancio un objeto del tipo SqlCommand
+            var objComando = new SqlCommand();
+
+            //Me conecto...
+            this.Conectar();
+
+
+            try
+            {
+                objComando.CommandText = pNombreStoreProcedure;
+                objComando.CommandType = CommandType.StoredProcedure;
+                objComando.Connection = this.objConexion;
+
+                if (pParametrosSql != null)
+                {
+                    //Lleno los SqlParameters a la lista de parametros
+                    objComando.Parameters.AddRange(pParametrosSql);
+                }
+
+                //Instancio un adaptador con el parametro SqlCommand
+                var objAdaptador = new SqlDataAdapter(objComando);
+
+                //Lleno la tabla, el objeto unaTabla con el adaptador
+                objAdaptador.Fill(unaTabla);
+            }
+            catch (Exception)
+            {
+                //Como hay error... por el motivo que sea asigno el resultado a null
+                unaTabla = null;
+
+                throw;
+            }
+            finally
+            {
+
+                //Pase lo que pase me desconecto
+                this.Desconectar();
+            }
+
+
+            return unaTabla;
+        }
+
+        public int EscribirPorStoreProcedure(string pTexto, SqlParameter[] pParametrosSql)
+        {
+            //Instanció una variable filasAfectadas que va a terminar devolviendo la cantidad de filas afectadas.
+            int filasAfectadas = 0;
+
+            //Instancio un objeto del tipo SqlCommand
+            var objComando = new SqlCommand();
+
+            //Me conecto...
+            this.Conectar();
+
+            try
+            {
+                objComando.CommandText = pTexto;
+                objComando.CommandType = CommandType.StoredProcedure;
+                objComando.Connection = this.objConexion;
+
+                if (pParametrosSql.Length > 0)
+                {
+                    objComando.Parameters.AddRange(pParametrosSql);
+                    //El método ExecuteNonQuery() me devuelve la cantidad de filas afectadas.
+                    filasAfectadas = objComando.ExecuteNonQuery();
+                }
+                else
+                {
+                    //retorno -1 porque la lista de parametros Sql tiene 0 ítems...
+                    filasAfectadas = -1;
+                }
+
+
+
+            }
+            catch (Exception)
+            {
+                filasAfectadas = -1;
+                throw;
+            }
+            finally
+            {
+                //Me desconecto
+                this.Desconectar();
+            }
+
+
+            return filasAfectadas;
+        }
+
 
     }
 }
